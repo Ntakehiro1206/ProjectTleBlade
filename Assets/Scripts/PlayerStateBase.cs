8c9b042d2533b8ea14f9f6af14deed74cc54f94d
LAPTOP-83635360\admin <m0a22221d0@edu,teu.ac.jp>
2025-04-23 17:37:13 +0900

    wip

--- Assets/Scripts/PlayerStateBase.cs
+++ Assets/Scripts/PlayerStateBase.cs
@@ -1,12 +1,32 @@
+using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
-using 
 
-public class IPlayerState
+public class PlayerStateBase: IPlayerState, IDisposable
 {
+    //初期化メソッド
+    public virtual void Initialize()
+    {
+        Debug.Log("PlayerStateBase Initialize");
+    }
     
-}
+    //破棄メソッド
+    public void Dispose()
+    {
+        Debug.Log($"{this.GetType().Name} is Dispose");
+    }
 
-public class PlayerStateBase : MonoBehaviour
-{
-    
+    //状態が始まったときの処理
+    public virtual async UniTask EnterAsync()
+    {
+        Debug.Log("PlayerStateBase EnterAsync");
+        await UniTask.CompletedTask;
+    }
+
+    //状態が終わるときの処理
+    public virtual async UniTask ExitAsync()
+    {
+        Debug.Log("PlayerStateBase ExitAsync");
+        await UniTask.CompletedTask;
+    }
 }
